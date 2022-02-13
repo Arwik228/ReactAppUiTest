f0fe3c150bc0559177e2e76bdcb12f770c20093e
Arwik228 <den_lebedev_2018@list.ru>
2022-02-13 15:15:36 +0500

    добавление карточки

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ScrollView} from 'react-native';
 import {Text, Layout} from '@ui-kitten/components';
 
+import CardBlock from './layout/CardBlock';
+
 export default () => {
+  const [object, setArrayCard] = React.useState({});
+  React.useEffect(() => {
+    fetch(`https://api.npoint.io/31877f7fc30d6283df09`)
+      .then(results => results.json())
+      .then(data => {
+        setArrayCard(data);
+      });
+  }, []);
+
   return (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text category="h1">Главная</Text>
+    <Layout style={styles.container}>
+      {'cards' in object ? (
+        <ScrollView horizontal={true} pagingEnabled={true}>
+          {object.cards.map((card, index) => (
+            <CardBlock card={card} lang={object.language} key={index} />
+          ))}
+        </ScrollView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  bottomNavigation: {
-    backgroundColor: '#f9f9f9',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f4f7',
   },
 });
---
+++ src/components/layout/CardBlock.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from 'react-native';
+import {Text, Layout, Card} from '@ui-kitten/components';
+import Tts from 'react-native-tts';
+
+const width = Dimensions.get('window').width;
+
+// Return  block
+export default props => {
+  const card = props.card;
+  Tts.setDefaultLanguage(props.lang);
+  return (
+    <Layout style={{justifyContent: 'center', backgroundColor: '#f2f4f7'}}>
+      <View style={styles.card}>
+        <Image source={{uri: card.image}} style={styles.image} />
+
+        <View style={styles.content}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text onPress={() => Tts.speak(card.word)} category="h2">
+              {card.word}
+            </Text>
+            <Text category="h4" style={{color: 'gray'}}> ({card.translate})</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              Tts.speak(card.text);
+            }}>
+            <Text style={{color: 'gray'}}>{card.text}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 10,
+    width: width - 20,
+    backgroundColor: '#ffffff',
+    borderRadius: 15,
+  },
+  image: {
+    width: width - 20,
+    height: 250,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  content: {
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    borderWidth: 1,
+    borderColor: '#e9eaf1',
+    padding: 10,
+  },
+});
